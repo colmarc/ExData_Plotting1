79b9116b127a61702562cef61a9dd41c2d84832f
colmarc <colmarc@hotmail.it>
2014-08-09 21:53:20 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,14 @@
+#this code creates the first plot and saves it as a png file
+
 png(filename = "plot1.png", 
     width = 480, height = 480,
     units = "px", bg = "transparent")
-hist(c(sub['Global_active_power'][,]),main='Global Active Power', col='red',xlab='Gloabl Active Power (Kilowatts)', ylim=range(0,1200), xlim=range(0,6))
+
+hist(sub['Global_active_power'][,],
+    main='Global Active Power', 
+    col='red',
+    xlab='Gloabl Active Power (Kilowatts)', 
+    ylim=range(0,1200), 
+    xlim=range(0,6))
+
 dev.off()
